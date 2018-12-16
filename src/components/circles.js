688ebc012e78069a843f3f40d681bc5b8e4f0f5b
theoleanse <theoleanse@gmail.com>
2018-12-16 13:05:09 +0000

    add text boxes

--- src/components/circles.js
+++ src/components/circles.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import * as R from 'ramda'
+import { path, compose, multiply, prop, map, pluck } from 'ramda'
 import * as d3 from 'd3'
 import { minusTwo, min, half, zeroIndexArrayOfLength, indexMap } from '../utils'
 
-const height = 1000
-const width = 1000
+const circumference = (height, n, x) => (height / n) * (n - x)
 
-const circumference = (n, height) => x => (height / n) * (n - x)
-
-const radius = (x, y) =>
-  R.compose(
-    minusTwo,
-    half,
-    circumference(x, y)
-  )
-
-const radii = (height, n) => R.map(radius(n, height), zeroIndexArrayOfLength(n))
+const radius = compose(
+  half,
+  circumference
+)
 
 const getPosition = (x, y) =>
-  R.path([x, y])({
+  path([x, y])({
     0: {
       0: 3,
       1: 2,
@@ -36,68 +29,147 @@ const getPosition = (x, y) =>
     },
   })
 
-const unit = R.compose(
+const unit = compose(
   min,
-  R.map(R.prop('r'))
+  pluck('r')
 )
 
-const data = indexMap(
-  (x, i) => ({
-    id: i,
-    x: half(width),
-    y: half(height),
-    r: x,
-    width: getPosition(0, i) * 4,
-  }),
-  radii(Math.min(width, height), 3)
-)
+const data = (width, height, n) =>
+  indexMap(
+    (x, i) => ({
+      id: i,
+      x: half(width),
+      y: half(height),
+      r: radius(Math.min(width - 10, height - 10), n, x),
+      width: multiply(4, getPosition(0, i)),
+    }),
+    zeroIndexArrayOfLength(n)
+  )
 
 const newData = data => id =>
-  R.map(
+  map(
     x => ({
       ...x,
-      r: getPosition(id, x.id) * unit(data),
+      r: multiply(unit(data), getPosition(id, x.id)),
       width: getPosition(id, x.id) * 4,
     }),
     data
   )
 
-const updater = data =>
-  d3
-    .select('svg')
+const updater = svg => data =>
+  svg
     .selectAll('circle')
     .data(data)
-    .transition(d3.transition().duration(750))
-    .attr('r', d => d.r)
-    .attr('stroke-width', ({ width }) => width)
+    .transition(
+      d3
+        .transition()
+        .duration(750)
+        .on('end', () => setUpNotches(svg))
+    )
+    .attr('r', prop('r'))
+    .attr('stroke-width', prop('width'))
 
-const handleClick = data =>
-  R.compose(
-    updater,
+const handleClick = (svg, data) =>
+  compose(
+    updater(svg),
     newData(data),
-    R.prop('id')
+    prop('id')
   )
 
-const setUpCircles = data =>
-  d3
-    .select('svg')
+const setUpCircles = (svg, data) =>
+  svg
     .selectAll('circle')
     .data(data)
     .enter()
     .append('circle')
     .attr('fill', 'none')
     .attr('stroke', 'black')
-    .attr('cx', d => d.x)
-    .attr('cy', d => d.y)
-    .attr('r', d => d.r)
-    .attr('stroke-width', d => d.width)
-    .on('click', handleClick(data))
+    .attr('cx', prop('x'))
+    .attr('cy', prop('y'))
+    .attr('r', prop('r'))
+    .attr('stroke-width', prop('width'))
+    .on('click', x => {
+      handleClick(svg, data)(x)
+      removeNotches(svg)
+    })
+
+const getAngle = (i, numNodes) => (i / (numNodes / 2)) * Math.PI
+const getX = radius => angle => radius * Math.cos(angle) + radius
+const getY = radius => angle => radius * Math.sin(angle) + radius
+const transpose = (totalWidth, radius) => x => x + totalWidth / 2 - radius
+
+const notch = (numNodes, radius, totalWidth = 1000) => i => ({
+  id: i,
+  x: compose(
+    transpose(totalWidth, radius),
+    getX(radius),
+    getAngle
+  )(i, numNodes),
+  y: compose(
+    transpose(totalWidth, radius),
+    getY(radius),
+    getAngle
+  )(i, numNodes),
+})
+
+const notches = (numberOfNotches, radius) =>
+  map(notch(numberOfNotches, radius), zeroIndexArrayOfLength(numberOfNotches))
+
+const getBoxX = (x, boxWidth, totalWidth = 1000) =>
+  x < totalWidth / 2 ? x + 15 : x - 15 - boxWidth
+
+const addInfoBox = svg => ({ x, y, boxWidth = 150 }) => {
+  svg
+    .append('rect')
+    .attr('x', getBoxX(x, boxWidth))
+    .attr('y', getBoxX(y, boxWidth))
+    .attr('width', boxWidth)
+    .attr('height', boxWidth)
+    .attr('fill', 'white')
+    .attr('stroke', 'black')
+  svg
+    .append('text')
+    .attr('x', getBoxX(x, boxWidth) + 3)
+    .attr('y', getBoxX(y, boxWidth) + 15)
+    .text('AK')
+}
+const removeInfoBox = svg => () => {
+  svg.select('rect').remove()
+  svg.select('text').remove()
+}
+
+const setUpNotches = svg => {
+  svg
+    .selectAll('circle')
+    .data(notches(7, 505))
+    .enter()
+    .append('svg:circle')
+    .attr('class', 'notch')
+    .attr('r', 5)
+    .attr('cx', prop('x'))
+    .attr('cy', prop('y'))
+    .attr('id', prop('id'))
+    .on('mouseover', addInfoBox(svg))
+    .on('mouseleave', removeInfoBox(svg))
+    .on('click', addInfoBox(svg))
+}
+
+const removeNotches = svg => svg.selectAll('circle.notch').remove()
 
 export default class GoodCircles extends React.Component {
+  constructor() {
+    super()
+    this.height = 1000
+    this.width = 1000
+    this.circleCount = 3
+  }
   componentDidMount() {
-    setUpCircles(data)
+    const initialState = data(this.width, this.height, this.circleCount)
+    const svg = d3.select('svg')
+    setUpCircles(svg, initialState)
+    setUpNotches(svg)
   }
   render() {
-    return <svg height={height} width={width} />
+    return <svg height={this.height} width={this.width} />
   }
 }
